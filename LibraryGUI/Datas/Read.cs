fb3daae2bc20984134a05e5c9ef898757281420d
Bencuska <rizb@kkszki.hu>
2026-02-04 10:31:36 +0100

    first commit

---
+++ LibraryGUI/Datas/Read.cs
@@ -0,0 +1,37 @@
+﻿using LibraryGUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryGUI.Datas
+{
+    internal class Read
+    {
+        public List<Authors> ReadAuthors()
+        {
+			using (var context = new librarydbContext())
+			{
+				var users = context.Authors.ToList();
+				return users;
+			}
+        }
+        public List<Books> ReadBooks()
+        {
+            using (var context = new librarydbContext())
+            {
+                var books = context.Books.ToList();
+                return books;
+            }
+        }
+        public List<Categories> ReadCategories()
+        {
+            using (var context = new librarydbContext())
+            {
+                var categories = context.Categories.ToList();
+                return categories;
+            }
+        }
+    }
+}
---
+++ LibraryGUI/Views/ShowDatas.xaml.cs
@@ -0,0 +1,50 @@
+﻿using LibraryGUI.Datas;
+using Org.BouncyCastle.Asn1.X509;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryGUI.Views
+{
+    /// <summary>
+    /// Interaction logic for ShowDatas.xaml
+    /// </summary>
+    public partial class ShowDatas : Page
+    {
+        Read read = new Read();
+        public ShowDatas()
+        { 
+            InitializeComponent();
+
+        }
+
+        private void szerzok_Click(object sender, RoutedEventArgs e)
+        {
+            var list = read.ReadAuthors();
+            dataGrid1.ItemsSource = read.ReadAuthors();
+        }
+
+        private void konyvek_Click(object sender, RoutedEventArgs e)
+        {
+            var list = read.ReadBooks();
+            dataGrid1.ItemsSource = read.ReadBooks();
+        }
+
+        private void kategoriak_Click(object sender, RoutedEventArgs e)
+        {
+            var list = read.ReadCategories();
+            dataGrid1.ItemsSource = read.ReadCategories();
+        }
+    }
+}
